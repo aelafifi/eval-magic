9d6ff8b37ee5808f699ca3e09d26cf0ea7ffe42f
ahmedelafifi-oc <ahmed.elafifi@oncorps.io>
2025-09-13 22:34:07 +0300

    Remove unneeded test files

--- playground/test3.1.ts
+++
@@ -1,9 +0,0 @@
-const arr = [undefined, 2, 3];
-const obj = { a: 1, b: 2, c: 3 };
-
-// const [a = 12, b, ...c] = arr;
-// console.log(a, b, c);
-
-const { a = 10, b = 20, d = 30, ...x } = obj;
-
-console.log(a, b, d, x);
