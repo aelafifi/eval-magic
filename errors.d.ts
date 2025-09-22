cb6d8b5e2bbf7eace366a03037e554b30ea31742
aelafifi <ahmed.s.elafifi@gmail.com>
2025-09-22 16:28:43 +0300

    Delete built files by mistake

--- errors.d.ts
+++
@@ -1,2 +0,0 @@
-export declare class PassToDefaultBehavior extends Error {
-}
--- errors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PassToDefaultBehavior = void 0;
-class PassToDefaultBehavior extends Error {
-}
-exports.PassToDefaultBehavior = PassToDefaultBehavior;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXJyb3JzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiZXJyb3JzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLE1BQWEscUJBQXNCLFNBQVEsS0FBSztDQUFHO0FBQW5ELHNEQUFtRCJ9
